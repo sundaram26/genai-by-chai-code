5234c51663a6427158d82c75b882f7c7737176db
sundaram26 <singhsundaram2004@gmail.com>
2026-03-06 23:31:37 +0530

    Created Training Corpus

--- src/features/tokenizer/utils/tokenizer.ts
+++ src/features/tokenizer/utils/tokenizer.ts
@@ -20,7 +20,46 @@ export class Tokenizer {
     }
 
     bpeEncode(text: string): number[] {
-        return [];
+        let tokens = utf8Encode(text);
+
+        const mergePairs = [...this.merges.entries()].sort((a, b) => a[1] - b[1]);
+
+        for (const [pair, newId] of mergePairs) {
+            const [first, second] = pair.split("-").map((val) => Number(val));
+            const newToken: number[] = [];
+
+            let i = 0;
+            while (i < tokens.length) {
+                if (
+                    i < tokens.length - 1 &&
+                    tokens[i] === first &&
+                    tokens[i + 1] === second
+                ) {
+                    newToken.push(newId);
+                    i += 2;
+                } else {
+                    newToken.push(tokens[i]);
+                    i++;
+                }
+            }
+            tokens = newToken;
+        }
+        return tokens;
     }
 
+    decode(tokens: number[]): string {
+        const bytes: number[] = [];
+
+        for (const token of tokens) {
+            const tokenBytes = this.vocab.get(token);
+            if (!tokenBytes) {
+                throw new Error(`Token ID ${token} not found in vocabulary`);
+            }
+            for (const byte of tokenBytes) {
+                bytes.push(byte);
+            }
+        }
+
+        return utf8Decode(bytes);
+    }
 }
